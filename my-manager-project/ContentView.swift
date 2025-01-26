8639befd58b222baee6acc794e1315b56fe3f794
gyoengit <gyoengnotion@gmail.com>
2025-01-26 22:26:45 +0900

    Initial Commit

---
+++ my-manager-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-manager-project
+//
+//  Created by 손희경 on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-manager-project/my_manager_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_manager_projectApp.swift
+//  my-manager-project
+//
+//  Created by 손희경 on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct my_manager_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
